7f703f065a8dbbbc87e6effde3532842f064be3b
Bryan José Mañon Colón <Bryann1820@gmail.com>
2023-08-25 12:21:40 -0400

    first commit

---
+++ config/dbConnect.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', false);
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("DB Connected Successfully");
+  } catch (error) {
+    console.log("DB Connection failed", error.message);
+  }
+};
+
+dbConnect();
